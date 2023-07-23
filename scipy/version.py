51a2b15609c2ed3c5c48537e55ddc24f57260b03
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-07-23 19:27:12 +0800

    alvistack/v1.11.1
        git clean -xdf
        git submodule sync
        git submodule update --init
        git submodule foreach git checkout -- .
        tar zcvf ../python-scipy_1.11.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-scipy.spec ../python-scipy_1.11.1-1.spec
        cp ../python*-scipy*1.11.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/scipy-scipy-1.11.1/
        rm -rf ../python*-scipy*1.11.1*.*
    
    See https://github.com/scipy/scipy/pull/18810
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ scipy/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING THE SCIPY BUILD
+# See tools/version_utils.py for details
+
+short_version = '1.11.1'
+version = '1.11.1'
+full_version = '1.11.1'
+git_revision = '9fe8acb'
+commit_count = '2266'
+release = True
+
+if not release:
+    version = full_version
